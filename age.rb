c77a1cd05f4d865fd8f3b8b91c97599f9eaf30b1
kasia-misirli <kasia.misirli@gmail.com>
2018-07-14 19:34:13 +1000

    ading age kata

---
+++ age.rb
@@ -0,0 +1,19 @@
+def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8)
+  array = []
+  total_age = 0
+  array << age_1 * age_1
+  array << age_2 * age_2
+  array << age_3 * age_3
+  array << age_4 * age_4
+  array << age_5 * age_5
+  array << age_6 * age_6
+  array << age_7 * age_7
+  array << age_8 * age_8
+  array.each do |age|
+    total_age += age
+  end
+    squared = Math.sqrt(total_age)
+    final = squared/2
+    return final.floor
+end
+predict_age(65,60,75,55,60,63,64,45)
